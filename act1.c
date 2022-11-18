7a2770e470a6fe687c685bff8f14a4536b062290
Sofia Paixão <anasofiapaixao>
2022-11-18 14:46:02 +0000

    class of 2022-11-18

---
+++ act1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	int val1=0, val2=0;
+	printf("input (syntax:val1<space>val2):");
+	scanf("%d %d", &val1, &val2);
+	printf("output: %d %d", val1, val2);
+	return 0;
+}
