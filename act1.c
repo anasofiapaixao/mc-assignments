d5cbdeb97bb2ddd05890a2dec5446523ea2b9407
Sofia Paixão <anasofiapaixao>
2022-11-18 15:01:57 +0000

    i apologize to my git history for my git history

--- act1.c
+++ act1.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 void main(void) {
-	int val1=0, val2=0;
-	printf("input (syntax:val1<space>val2):");
-	scanf("%d %d", &val1, &val2);
-	printf("output: %d %d", val1, val2);
+	int a=0, b=0;
+	printf("input (syntax: value1<space>value2):");
+	scanf("%d %d", &a, &b);
+	printf("output: %d %d", a, b);
 }
